da3dafbab12db7e40a32503d561e123243926111
zhonminlin <166679605+zhonminlin@users.noreply.github.com>
2024-05-30 11:02:38 +0800

    Update 3.3.py

--- 3.3.py
+++ 3.3.py
@@ -3,18 +3,22 @@ pass_count = 0
 fail_count = 0
 total_score = 0
 
-
 while True:
     score = int(input("請輸入學生成績（輸入 -1 表示結束）："))
+    
     if score == -1:
         break
+    
+    if score < 0 or score > 100:
+        print("請輸入有效成績（0-100之間）")
+        continue
+    
     total_students += 1
     total_score += score
     if score >= 60:
         pass_count += 1
     else:
         fail_count += 1
-
 
 print("全班人數：", total_students)
 print("及格人數：", pass_count)

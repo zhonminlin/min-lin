586ee589f0b9d29ff9c228f9ba059e958005a47e
zhonminlin <166679605+zhonminlin@users.noreply.github.com>
2024-05-30 10:48:05 +0800

    Add files via upload

---
+++ 3.3.py
@@ -0,0 +1,26 @@
+total_students = 0
+pass_count = 0
+fail_count = 0
+total_score = 0
+
+
+while True:
+    score = int(input("請輸入學生成績（輸入 -1 表示結束）："))
+    if score == -1:
+        break
+    total_students += 1
+    total_score += score
+    if score >= 60:
+        pass_count += 1
+    else:
+        fail_count += 1
+
+
+print("全班人數：", total_students)
+print("及格人數：", pass_count)
+print("不及格人數：", fail_count)
+if total_students > 0:
+    average_score = total_score / total_students
+    print("全班平均值：", average_score)
+else:
+    print("無學生成績")
